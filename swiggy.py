fc4f92876e49630fea4a324deb8fb0415cb5b5cf
Nanthakumar J J <jjnanthakumar477@gmail.com>
2022-07-22 11:49:14 +0530

    Modified requirements and .gitignore

--- swiggy.py
+++ swiggy.py
@@ -4,15 +4,16 @@ import sys
 from rich import print
 
 HEADERS = {
-    'Host':'www.swiggy.com',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36',
-    'Accept':'*/*',
+    'Host': 'www.swiggy.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36',
+    'Accept': '*/*',
     'Accept-Language': 'en-US,en;q=0.5',
     'Referer': 'https://www.swiggy.com/my-account/orders',
     'Content-Type': 'application/json'
-    }
+}
 
 GET_ORDERS_URL = 'https://www.swiggy.com/dapi/order/all?order_id='
+
 
 def getOrders(cookies):
     print("Retrieving...")
@@ -23,38 +24,37 @@ def getOrders(cookies):
     while 1:
         # 10 orders retrieved in each api call
         URL = ''
-        if last_order_id!='':
+        if last_order_id != '':
             URL = GET_ORDERS_URL+str(last_order_id).strip()
         else:
             URL = GET_ORDERS_URL
 
         r = s.get(URL, headers=HEADERS, cookies=cookies)
         resp = json.loads(r.text)
-        if resp['statusCode']==1:
+        if resp['statusCode'] == 1:
             print("[red][-] Status Code is 1, exiting[/red]")
             break
 
-        if len(resp['data']['orders'])==0:
+        if len(resp['data']['orders']) == 0:
             print("Reached end of orders")
             break
         for order in resp['data']['orders']:
             order_id = order['order_id']
             order_total = order['order_total']
             # print(order_total)
-            num_of_orders+=1
-            spent+=order_total
-        
+            num_of_orders += 1
+            spent += order_total
 
         last_order_id = resp['data']['orders'][-1]['order_id']
-    
+
     average_spent = spent//num_of_orders
     print()
-    print(f"[green]Total money spent on swiggy.com : [bold]INR {spent:,}[/bold][/green]")
-    print(f"[green]Total number of orders placed : [bold]{num_of_orders:,}[/bold][/green]")
-    print(f"[green]Average money spent on each order : [bold]INR {average_spent:,}[/bold][/green]")
-
-
-
+    print(
+        f"[green]Total money spent on swiggy.com : [bold]INR {spent:,}[/bold][/green]")
+    print(
+        f"[green]Total number of orders placed : [bold]{num_of_orders:,}[/bold][/green]")
+    print(
+        f"[green]Average money spent on each order : [bold]INR {average_spent:,}[/bold][/green]")
 
 
 def cookiesToDict():
@@ -62,13 +62,13 @@ def cookiesToDict():
     data = None
     cookies = {}
     try:
-        with open("cookies.json","r") as f:
+        with open("cookies.json", "r") as f:
             data = json.load(f)
     except Exception as e:
         print("[red][-] [u]cookies.json[/u] not found in the path[/red]")
         print(str(e))
         return None
-    
+
     try:
         for i in data:
             cookies[i['name']] = i['value']
@@ -77,7 +77,7 @@ def cookiesToDict():
         print(str(e))
         return None
 
-    return cookies 
+    return cookies
 
 
 def checkLogin(cookies):
@@ -97,7 +97,7 @@ def checkLogin(cookies):
     if resp['statusCode'] == 1:
         print("[red][-] Not logged in, check cookies and try again[/red]")
         return False
-    
+
     return True
 
 
@@ -108,5 +108,3 @@ if __name__ == "__main__":
         sys.exit()
     if checkLogin(cookies):
         getOrders(cookies)
-    
-
